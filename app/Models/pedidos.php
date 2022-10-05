ec564f4933f62e8e5b8f88d07751154b90e02caf
danielthejoker18 <danielthejoker18@gmail.com>
2022-10-04 22:54:47 -0300

    :wrench: configuração de parametros para sort da classe pedidos

--- app/Models/pedidos.php
+++ app/Models/pedidos.php
@@ -4,24 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class pedidos extends Model
 {
     use HasFactory;
-
+    use Sortable;
     protected $fillable = [
         "clientes_id",
         "data_pedido",
         "status"
     ];
 
+    public $sortable = ['clientes_id', 'data_pedido', 'status'];
+
     public function pedido()
     {
-        return $this->belongsTo(clientes::class);
+        return $this->belongsTo(clientes::class, 'clientes_id', 'clientes_id');
     }
 
     public function pedido_produto()
     {
-        return $this->hasMany(pedido_produto::class);
+        return $this->hasMany(pedido_produto::class, 'produtos_id', 'id');
     }
 }
