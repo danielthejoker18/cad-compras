2be88c93055b97506b987bae7fdc2ac59c00a427
danielthejoker18 <danielthejoker18@gmail.com>
2022-10-04 22:49:28 -0300

    :wrench: paginação alterada para 20 itens

--- app/Http/Controllers/clientesController.php
+++ app/Http/Controllers/clientesController.php
@@ -14,9 +14,27 @@ class clientesController extends Controller
     public function index()
     {
         Paginator::useBootstrap();
-        $clientes = clientes::sortable()->paginate(10);
+        $clientes = clientes::sortable()->paginate(20);
         return view('clientes', compact('clientes'));
     }
+
+    public function editar($id)
+    {
+        $cliente = clientes::findOrFail($id);
+        return view('editar_cliente', ['cliente' => $cliente]);
+    }
+
+    public function update($id, Request $request)
+    {
+        try {
+            $cliente = clientes::findOrFail($id);
+            $cliente->update($request->all());
+        } catch (Exception $e) {
+            return Redirect::to('/clientes')->with('falha', 'Falha ao atualizar registro!');
+        }
+        return Redirect::to('/clientes')->with('sucesso', 'Registro Atualizado com sucesso!');
+    }
+
 
     public function delete($id)
     {
