17967aeed92440f3ad39888406ae970f9032d801
danielthejoker18 <danielthejoker18@gmail.com>
2022-10-04 22:58:33 -0300

    :wrench: adicionadas rotas para pedidos e para edição de clientes

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*Rotas de cliente*/
+
 Route::get('/', 'clientesController@index');
 Route::get('/clientes', 'clientesController@index');
 Route::delete('/delete/{id}', 'clientesController@delete');
-Route::get('/editar_cliente', function () {
-    return view('editar_cliente');
-});
+Route::get('/editar_cliente/{id}', 'clientesController@editar');
+Route::post('update/{id}', 'clientesController@update');
+/*Rotas de cliente*/
 
-Route::get('/produtos', function () {
-    return view('produtos');
-});
-
-Route::get('/pedidos', function () {
-    return view('pedidos');
-});
+/*Rotas de pedidos*/
+Route::get('/pedidos', 'pedidosController@index');
+/*Rotas de cliente*/
