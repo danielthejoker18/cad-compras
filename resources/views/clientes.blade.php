27b6ca64fe44bfb9e0dcce5b89e7b6129408caaa
danielthejoker18 <danielthejoker18@gmail.com>
2022-10-04 22:59:19 -0300

    :wrench: adição de nav padrão na view de clientes

--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -13,9 +13,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
 </head>
+@include('navbar');
 
 <body class="">
-    <div class="container">
+    <div class="container pr-5 pl-5">
+        <div class="pt-2 pl-2">
+            <a class="btn btn-primary mb-2" href="add/cliente">Adicionar Cliente</a>
+        </div>
         @if (session('falha'))
         <div class="alert alert-danger alert-dismissible" role="alert">
             <div>{{ session('falha') }}</div>
@@ -28,19 +32,19 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
-        <table class="table">
-            <thead>
+        <table class="table table-bordered table-striped table-hover ">
+            <thead class="thead-dark">
                 <tr>
                     <th scope="col">@sortablelink('id') <span class="span float-right"><i class="bi bi-arrow-down-up"></i></span></th>
                     <th scope="col">@sortablelink('cpf')</th>
-                    <th scope="col">@sortablelink('nome_cliente')</th>
-                    <th scope="col">@sortablelink('created_at')</th>
+                    <th scope="col">@sortablelink('nome_cliente', 'Nome')</th>
+                    <th scope="col">@sortablelink('created_at', 'Criado Em')</th>
                     <th>editar</th>
                     <th>deletar</th>
                 </tr>
             </thead>
 
-            <tbody>
+            <tbody class="table-group-divider">
                 @if($clientes->count())
                 @foreach($clientes as $u)
                 <tr>
@@ -48,7 +52,7 @@
                     <td>{{$u->cpf}}</td>
                     <td>{{$u->nome_cliente}}</td>
                     <td>{{$u->created_at}}</td>
-                    <td><a class="btn btn-secondary" href="{{url('editar_cliente')}}">Editar</a></td>
+                    <td><a class="btn btn-secondary" href="{{url('editar_cliente')}}/{{$u->id}}">Editar</a></td>
                     <td>
                         <form action="{{url('/delete')}}/{{$u->id}}" method="post">
                             @csrf
