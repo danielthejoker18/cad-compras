e84f45a978fea135a41cf4fccf5b1c7f7e4628cc
danielthejoker18 <danielthejoker18@gmail.com>
2022-10-04 23:00:07 -0300

    :wrench: ajustados campos de valor da edição de cliente

--- resources/views/editar_cliente.blade.php
+++ resources/views/editar_cliente.blade.php
@@ -5,20 +5,20 @@
 
 <body>
     <div class="container">
-        <form action="{{url('edita_user')}}" method="post" class="col-6 position-absolute top-50 start-50 translate-middle">
+        <form action="{{url('update')}}/{{$cliente->id}}" method="post" class="col-6 position-absolute top-50 start-50 translate-middle">
             @csrf
             <div class="mb-3">
                 <label for="exampleInputEmail1" name="cpf" class="form-label">CPF</label>
-                <input type="text" class="form-control" name="cpf">
+                <input type="text" class="form-control" name="cpf" value="{{$cliente->cpf}}">
                 <div class="form-text">digite o cpf</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Nome</label>
-                <input type="text" name="nome" class="form-control">
+                <input type="text" name="nome_cliente" class="form-control" value="{{$cliente->nome_cliente}}">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">E-mail</label>
-                <input type="email" name="email" class="form-control">
+                <input type="email" name="email" class="form-control" value="{{$cliente->email}}">
             </div>
             <a href="{{url('clientes')}}" class="btn btn-secondary ">Voltar</a>
             <button type="submit" class="btn btn-success ">Salvar</button>
